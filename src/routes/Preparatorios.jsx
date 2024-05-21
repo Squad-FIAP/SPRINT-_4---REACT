2f0338438f18f71ab4c7e0832f160564fe19d238
Vinicius <viniss-starwars@hotmail.com>
2024-05-21 16:45:59 -0300

    comitando scss

--- src/routes/Preparatorios.jsx
+++ src/routes/Preparatorios.jsx
@@ -1,7 +1,18 @@
+import '../scss/estilo.scss'
+
 function Preparatorios() {
     return(
         <>
-
+        <main>
+            <div className="video-cointainer">
+                <div className="video-section">
+                    <h1>Aprendendo as profissões brincando de médico</h1>
+                    <div className="video-container">
+                        <iframe id="video-1" width="560" height="315" src="https://www.youtube.com/embed/0Zxdc1mlgBI?si=FetnroxqEclwaRIx" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>
+        </main>
         </>
     )
 }
