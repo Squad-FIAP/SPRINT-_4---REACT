d8faa1c713fc60f4b02364ecd93227241e653a3c
Vinicius <viniss-starwars@hotmail.com>
2024-05-21 17:58:33 -0300

    estilizacao pagina Erro

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,7 +1,9 @@
 function Error() {
     return (
         <>
-        <h1>404-Página não encontrada !!!</h1>
+        <section className="paginaErro">
+            <h1 className='tituloErro'>404-Página não encontrada !!!</h1>
+        </section>
         </>
     )
 }
